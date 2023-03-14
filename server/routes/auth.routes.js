1d52a27883f1c6f2f12bcabdfa784e5306d9c481
Omar AMRI <amri.omar93@gmail.com>
2023-03-14 19:34:42 +0100

    Confirm Account

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { login, logout, refreshToken, revoke } = require('../controllers/auth.controller')
+const { login, logout, refreshToken, revoke, confirmAccount } = require('../controllers/auth.controller')
 router.post('/login', login);
 router.post('/logout', logout);
 router.post('/revoke/:id', revoke)
 router.post('/refresh-token', refreshToken)
+router.get('/confirm/:token', confirmAccount)
 
 module.exports = router;
