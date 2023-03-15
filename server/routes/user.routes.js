9c8268a9ac8ed74a976c263fd51b433a3bb21c36
abdou6666 <abdousfayhi12@gmail.com>
2023-03-15 17:19:23 +0100

    added upload photo to cloudinary

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createUser, deleteUser, getUser, getUsers, updateUser, resetPassword, banAccount, updateRole } = require("../controllers/user.controller")
+const { createUser, deleteUser, getUser, getUsers, updateUser, resetPassword, banAccount, updateRole, uploadPhoto } = require("../controllers/user.controller")
 const validationMiddleware = require('../middlewares/validation.middleware')
 const { object, string, } = require('yup');
 const authenticationMiddleware = require('../middlewares/auth.middleware');
 const hasRoleMiddleware = require('../middlewares/hasRole.middleware');
+const upload = require("../configs/multer.config");
 
 
 const createUserValidationSchema = object({
@@ -37,6 +38,7 @@ router.get('/', getUsers)
 router.put('/ban/:id', authenticationMiddleware, hasRoleMiddleware('admin'), banAccount)
 
 router.put('/update/role/:id', authenticationMiddleware, hasRoleMiddleware('admin'), validationMiddleware(updateRoleSchema), updateRole)
+router.post('/update/photos/:userId', authenticationMiddleware, upload.single('image'), uploadPhoto)
 
 router.delete('/:id', authenticationMiddleware, hasRoleMiddleware('admin'), deleteUser)
 
